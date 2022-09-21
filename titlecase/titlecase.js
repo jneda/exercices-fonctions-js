f425ba4995fe90630fdadd6f1300f3c841ac67b4
jneda <julien.dane@gmail.com>
2022-09-21 19:02:50 +0200

    Implémenté titleCase

--- titlecase/titlecase.js
+++ titlecase/titlecase.js
@@ -1,1 +1,24 @@
-/* 3/ Écrivez une fonction JavaScript qui accepte une chaîne comme paramètre et convertit la première lettre de chaque mot de la chaîne en majuscule. */
+/* 3/ Écrivez une fonction JavaScript qui accepte une chaîne comme paramètre et
+convertit la première lettre de chaque mot de la chaîne en majuscule. */
+
+function toTitleCase(chaine) {
+  const mots = chaine.split(" ");
+  for (let i = 0; i < mots.length; i++) {
+    const mot = mots[i];
+    mots[i] = mot.charAt(0).toUpperCase() + mot.slice(1);
+  }
+  return mots.join(" ");
+}
+
+/* TODO: essayer avec Array.prototype.forEach(),
+ou mieux sans doute, Array.prototype.map() */
+
+// tests
+function log(x) {
+  console.log(x, " => ", toTitleCase(x));
+}
+
+log("coucou !");
+log ("coucou maman !");
+log("coucou maman je suis développeur web !");
+log("");
