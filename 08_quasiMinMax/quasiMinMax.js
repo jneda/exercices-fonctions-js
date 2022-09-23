a5e1243fc153788a51c7bde5010534400f810ad0
jdane <julien.dane@gmail.com>
2022-09-23 09:40:23 +0200

    Définition de la fonction de tri

--- 08_quasiMinMax/quasiMinMax.js
+++ 08_quasiMinMax/quasiMinMax.js
@@ -4,6 +4,16 @@ et le deuxième plus grand nombre.
 Ex : let arr = [1,2,3,4,5]
 Sortie prévue : 2,4 */
 
+// fonction utilitaire
+
+function numSort(array) {
+  // NB : définir la fonction de tri sans quoi les nombres négatifs
+  // seront triés alphabétiquement
+  // NB2 : cette fonction modifie l'array donné en entrée,
+  // donc pas de valeur de retour
+  array.sort((a, b) => a - b)
+}
+
 function quasiMinMax(nombres) {
   // on sort si la condition n'est pas respectée
 
@@ -12,7 +22,8 @@ function quasiMinMax(nombres) {
   }
 
   // faisons simple : on trie le tableau en ordre croissant
-  nombres.sort();
+  
+  numSort(nombres);
 
   // et on retourne la seconde valeur et l'avant-dernière
 
@@ -29,3 +40,4 @@ log([3, 1, 2]);
 log([26,42]);
 log([666]);
 log([]);
+log([1, 2, 3, 4, 5, -1, -2, -3, -4, -5]);
